92123c4d35cc78098f3aa051c9d4498a7ec3cf0b
NakChoi <nakchoi94@gmail.com>
2023-07-31 22:27:17 +0900

    # TIL : HTML 연습

--- TIL/StudyFinal/src/Main.java
+++ TIL/StudyFinal/src/Main.java
@@ -1,3 +1,6 @@
+/*
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -8,5 +11,45 @@ public class Main {
         bus.val.intVal = 3;
 
         System.out.println(car.intVal);
+
+        Scanner in = new Scanner(System.in);
+
+        in.
+
+                // A+B, A-B, A*B, A/B(몫), A%B(나머지)
     }
 }
+*/
+
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        /*String A = in.nextLine();
+
+        String[] arr = A.split(" ");
+
+        Long sum = 0L;
+
+        for(String i : arr ){
+            sum += Long.parseLong(i);
+        }
+*/
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+
+
+
+
+    }
+}
+
+
