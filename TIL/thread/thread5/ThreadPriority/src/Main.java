383b386c1d2414e0f0336d5a5bd0c4fcfa70b69f
NakChoi <nakchoi94@gmail.com>
2023-08-09 18:27:19 +0900

    TIL: thread의 우선순위

---
+++ TIL/thread/thread5/ThreadPriority/src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+
+        ThreadEx5_1 t1 = new ThreadEx5_1();
+        ThreadEx5_1 t2 = new ThreadEx5_1();
+
+        t2.setPriority(7);
+
+
+        System.out.println("Priority of th1(-) : " + t1.getPriority());
+        System.out.println("Priority of th1(|) : " + t2.getPriority());
+
+        t1.start();
+        t2.start();
+
+    }
+}
+
+
+class ThreadEx5_1 extends Thread{
+    public void run() {
+        for (int i = 0; i < 300; i++) {
+            System.out.print("-");
+            for (int x = 0; x < 10000000; x++) { // 작업을 지연 시키기 위한 for문
+
+            }
+        }
+    }
+}
+
+class ThreadEx5_2 extends Thread{
+    public void run() {
+        for (int i = 0; i < 300; i++) {
+            System.out.print("|");
+            for (int x = 0; x < 10000000; x++) { // 작업을 지연 시키기 위한 for문
+
+            }
+        }
+    }
+}
