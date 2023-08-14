567eb45676cd7ecf21d00ca6799f82ac3898a910
NakChoi <nakchoi94@gmail.com>
2023-08-14 15:55:21 +0900

    TIL: fork() and join() Practice

--- TIL/thread/thread8/waitAndNotify/src/Main.java
+++ TIL/thread/thread8/waitAndNotify/src/Main.java
@@ -8,6 +8,8 @@ public class Main {
 
         Table table = new Table();
 
+
+
         new Thread(new Cook(table), "COOK1").start();
         new Thread(new Customer(table, "donut"), "CUST1").start();
         new Thread(new Customer(table, "burger"), "CUST2").start();
