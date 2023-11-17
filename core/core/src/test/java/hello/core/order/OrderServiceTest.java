c8f018caa008af4e17ed838909299edff1a450e4
nakchoi <nakcho94@gmail.com>
2023-11-17 14:51:53 +0900

    2023.11.17.금요일 2-4 주문과 할인 실행과 테스트

---
+++ core/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,25 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceimpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L; // primitive type에는 null이 들어갈 수 없어어 long이 아닌 Long 타입을 했다.
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+
+    }
+}
